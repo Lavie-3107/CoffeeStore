0044793f4d4811418ea8a531486ae5e216d2ebbe
anhtu-devfe <nguyenanhtu2204tb@gmail.com>
2023-02-09 00:03:15 +0700

    style product detail

--- screen/productDetail/productDetail.tsx
+++ screen/productDetail/productDetail.tsx
@@ -1,28 +1,40 @@
 import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native"
 import { TypeProductDetail } from "./interface"
-import  Icon  from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/Ionicons'
+import Iconn from 'react-native-vector-icons/AntDesign'
 const ProductDetail:React.FC<TypeProductDetail>=({
 
 })=>{
   return(
     <SafeAreaView>
       <View style={{position:"relative"}}>
-        <Image source={{uri:"https://img.meta.com.vn/Data/image/2019/07/31/capuchino-la-gi-cach-uong-capuchino-2.jpg"}} style={{height:400,width:"100%",resizeMode:"cover",borderRadius:40}}/>
+        <Image source={{uri:"https://vcdn1-dulich.vnecdn.net/2021/09/10/caphe-3391-1631291456.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=hBCk64sCNamz5Ha8DDig0Q"}} style={{height:400,width:"100%",resizeMode:"cover",borderRadius:40}}/>
         <View style={styles.wrapinfoProduct}>
-          <View style={{backgroundColor:"#444444",width:"100%",zIndex:5}}/>
-          <View style={{flexDirection:"row",width:"100%",backgroundColor:"#ffffff"}}>
-            <View>
+          <View style={styles.backGroundBlur} />
+          <View>
+            
+          </View>
+          <View style={{zIndex:5,paddingHorizontal:27,paddingVertical:25}}>
+            <View style={{flexDirection:"row",justifyContent:"space-between"}}>
+
               <View>
-                <Text>Espresso</Text>
-                <Text>with chocolate</Text>
+                <Text style={styles.textName}>Espresso</Text>
+                <Text style={styles.textSubname}>with chocolate</Text>
               </View>
+              
               <View>
-                <View>
+                <View style={{flexDirection:"column",alignItems:"center"}}>
                 <Icon name="water" size={25} color="#ffffff"/>
-                <Text>Coffee</Text>
+                  <Text style={ styles.ingredient}>Coffee</Text>
                 </View>
               </View>
             </View>
+
+            <View style={{flexDirection:"row",gap:5,marginTop:21}}>
+               <Iconn name="star" color="#D17842" size={16} />
+                <Text style={ styles.numberStar}>4.6</Text>
+            </View>
+
           </View>
         </View>
       </View>
@@ -30,11 +42,53 @@ const ProductDetail:React.FC<TypeProductDetail>=({
   )
 }
 const styles=StyleSheet.create({
-  wrapinfoProduct:{
+  wrapinfoProduct: {
+    width:"100%",
     position:"absolute",
     bottom:0,
     zIndex:3,
-    backgroundColor:"#ffffff"
+  },
+  backGroundBlur: {
+    backgroundColor: "#333333",
+    width: "100%",
+    zIndex: 5,
+    height: "100%",
+    position: "absolute",
+    bottom: 0,
+    opacity: 0.8,
+    borderRadius:40
+  },
+  textName: {
+    fontFamily: "SF Pro Text",
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: 24,
+    lineHeight: 24,
+    color:"#ffffff"
+  },
+  textSubname: {
+    fontFamily: "SF Pro Text",
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: 12,
+    lineHeight: 18,
+    color:"#ffffff"
+  },
+  ingredient: {
+    fontFamily: "SF Pro Text",
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: 12,
+    lineHeight: 18,
+    color:"#ffffff"
+  },
+  numberStar: {
+     fontFamily: "SF Pro Text",
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: 16,
+    lineHeight: 18,
+    color:"#ffffff"
   }
 })
 export default ProductDetail
