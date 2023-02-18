2a4b54367fd4d8afb72170288e632e680cfcb5bb
anhtu-devfe <nguyenanhtu2204tb@gmail.com>
2023-02-19 00:26:06 +0700

    add search product

--- screen/home/homeScreen.tsx
+++ screen/home/homeScreen.tsx
@@ -1,17 +1,32 @@
-import { SafeAreaView,StyleSheet,Text,View,Image, ScrollView, FlatList } from "react-native"
+import { SafeAreaView,StyleSheet,Text,View,Image, ScrollView, FlatList, TouchableOpacity } from "react-native"
 import { HomeScreenType } from "./interface"
 import Header from "../../src/components/header/index"
 import Input from "../../src/components/input/input"
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import Icon from 'react-native-vector-icons/FontAwesome'
 import Dropdown from "../../src/components/dropdown/dropDown"
 import BlockListProdTabHome from "../../src/components/blockListProdTabHomescreen"
 import BlockListProHome from "../../src/components/blockListProHomesreen"
-import {styles} from "./styles"
+import { styles } from "./styles"
+import { data } from "./fakeData"
+import { useDispatch } from "react-redux"
+import { pushDataProductsDetail } from "../../redux/productsDetail/productsDetailSlice"
 const HomeScreen: React.FC<HomeScreenType> = ({
         navigation
 }) => {
+    const dispatch=useDispatch()
     const [valueInput, setValueInput] = useState<string>("")
+    const [productSearch,setProductSearch]=useState([])
+    const searchText = (text: string) => {
+        const productSearch:any=data.filter((el:any)=>`${el.name} ${el.subname}`.toLocaleLowerCase().includes(text.toLocaleLowerCase()))
+        setValueInput(text)
+        setProductSearch(productSearch)
+    }
+    const handleRedirectProDetaill = (items:any) => {
+        dispatch(pushDataProductsDetail(items))
+        navigation.navigate("ProductDetail")
+        setValueInput("")
+    }
     return (
         <ScrollView>
             <SafeAreaView>
@@ -23,10 +38,29 @@ const HomeScreen: React.FC<HomeScreenType> = ({
                 </View>
                 <View style={styles.warpInputFilter}>
                     <View style={styles.wrapInput}>
-                        <Input placeholder="Find your coffee..." classesInput={styles.stylesInput} value={valueInput} onChange={setValueInput} />
-                        <Icon style={styles.icon} name="search" size={15} color="#808080"/>
-                    </View>
-                    <Dropdown type="icon" classDropdown={styles.dropdown} />
+                        <Input placeholder="Find your coffee..." classesInput={styles.stylesInput} value={valueInput} onChange={searchText} />
+                            <Icon style={styles.icon} name="search" size={15} color="#808080" />
+                            {valueInput !== "" && <View style={{ width: "100%" }}>
+                                <FlatList
+                                contentContainerStyle={{gap:10}}
+                                data={productSearch}
+                                renderItem={({item,index}:any)=>{
+                                return  <TouchableOpacity style={{
+                                                            width: "100%",
+                                                            borderBottomWidth: 1,
+                                                            borderColor: "rgba(0, 0, 0, 0.1)",
+                                                            paddingVertical:5
+                                                            
+                                                }}
+                                                    key={index} onPress={()=>handleRedirectProDetaill(item)}
+                                                >
+                                                            <Text>{ `${item.name as any} ${item.subname}`}</Text>
+                                                    </TouchableOpacity>
+                            }}
+                            >
+                            </FlatList>
+                            </View>      }            
+                        </View>
                 </View>
                     <BlockListProdTabHome navigation={navigation } />
                     <BlockListProHome title="Popular Now" navigation={navigation }/>
--- screen/home/styles.js
+++ screen/home/styles.js
@@ -1,58 +1,47 @@
 import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
-  wrapInput: {
-      position:"relative"
-  },
-  wrapAllContent: {
-      marginTop: 35,
-      paddingHorizontal:23
-  },  
-  wrapTitle: {
-  },
-  textTitle: {
-      fontFamily: "SF Pro Text",
-      fontStyle: "normal",
-      fontWeight:"700",
-      fontSize: 22,
-      lineHeight: 32,
-      color:"#444444"
-  },
-  warpInputFilter: {
-      flexDirection: "row",
-      gap: 11,
-      marginTop: 25,
-      alignItems: "center",
-      justifyContent:"space-between"
-  },
-  stylesInput: {
-      width: 290,
-      height: 50,
-      backgroundColor: "#ffffff",
-      borderWidth: 1,
-      borderColor: "rgba(0, 0, 0, 0.1)",
-      borderRadius: 15,
-      elevation: 2,
-      paddingVertical: 18,
-      paddingLeft: 32,
-      paddingRight: 18,
-      fontWeight: "700",
-  },
-  icon: {
-      position: "absolute",
-      top: 16,
-      left:10
-      
-  },
-  dropdown: {
-      width: 50,
-      height: 50,
-      backgroundColor: "#967259",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      borderTopLeftRadius: 25,
-      borderTopRightRadius: 25,
-      borderBottomLeftRadius: 15,
-      borderBottomRightRadius:30
-  },
+    wrapInput: {
+        width: "100%",
+        position: "relative"
+    },
+    wrapAllContent: {
+        marginTop: 35,
+        paddingHorizontal: 23
+    },
+    wrapTitle: {
+    },
+    textTitle: {
+        fontFamily: "SF Pro Text",
+        fontStyle: "normal",
+        fontWeight: "700",
+        fontSize: 22,
+        lineHeight: 32,
+        color: "#444444"
+    },
+    warpInputFilter: {
+        width: "100%",
+        flexDirection: "row",
+        gap: 11,
+        marginTop: 25,
+        alignItems: "center",
+        justifyContent: "space-between"
+    },
+    stylesInput: {
+        height: 50,
+        backgroundColor: "#ffffff",
+        borderWidth: 1,
+        borderColor: "rgba(0, 0, 0, 0.1)",
+        borderRadius: 15,
+        elevation: 2,
+        paddingVertical: 18,
+        paddingLeft: 32,
+        paddingRight: 18,
+        fontWeight: "700",
+    },
+    icon: {
+        position: "absolute",
+        top: 16,
+        left: 10
+
+    },
 })
